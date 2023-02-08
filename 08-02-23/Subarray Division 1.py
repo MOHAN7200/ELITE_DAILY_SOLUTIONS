27ff487ba946478d844e8ddc0ce6fd5315377a98
MOHAN7200 <85169368+MOHAN7200@users.noreply.github.com>
2023-02-08 10:34:27 +0530

    Create Subarray Division 1.py

---
+++ 08-02-23/Subarray Division 1.py
@@ -0,0 +1,13 @@
+#Subarray Division 1
+
+
+def birthday(s, d, m):
+    count=0
+    if(len(s)==1 and s[0]==d):
+        return 1
+        
+    for i in range(len(s)-m+1):
+        print(s[i:i+m])
+        if(sum(s[i:i+m])==d):
+            count+=1
+    return count
